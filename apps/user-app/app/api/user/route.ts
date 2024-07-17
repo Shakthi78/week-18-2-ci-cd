5f1d644342d07c707e64390dbb294e2ed31b63e2
Shakthi <shakthiraj577@gmail.com>
2024-07-17 15:30:19 +0530

    space added

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 
-
 export const GET = async () => {
     const session = await getServerSession(authOptions);
     if (session.user) {
